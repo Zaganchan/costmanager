ba0c93641a2c43f13dc17abecbf15c6fdbe0fc22
Zagan <hakoniwa0311@gmail.com>
2018-05-13 01:14:26 +0900

    ログイン機構 要員マスタ完成

---
+++ cms/models.py
@@ -0,0 +1,152 @@
+from django.conf import settings
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
+
+# カスタムUserモデルを使う場合だけ、登録する
+if settings.AUTH_USER_MODEL == 'cms.User':
+    class UserManager(BaseUserManager):
+        """ユーザーマネージャー"""
+        use_in_migrations = True
+
+        def _create_user(self, email, password, **extra_fields):
+            """Create and save a user with the given username, email, and
+            password."""
+            if not email:
+                raise ValueError('The given email must be set')
+            email = self.normalize_email(email)
+
+            user = self.model(email=email, **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+        def create_user(self, email, password=None, **extra_fields):
+            extra_fields.setdefault('is_staff', False)
+            extra_fields.setdefault('is_superuser', False)
+            return self._create_user(email, password, **extra_fields)
+
+        def create_superuser(self, email, password, **extra_fields):
+            extra_fields.setdefault('is_staff', True)
+            extra_fields.setdefault('is_superuser', True)
+
+            if extra_fields.get('is_staff') is not True:
+                raise ValueError('Superuser must have is_staff=True.')
+            if extra_fields.get('is_superuser') is not True:
+                raise ValueError('Superuser must have is_superuser=True.')
+
+            return self._create_user(email, password, **extra_fields)
+
+
+    class User(AbstractBaseUser, PermissionsMixin):
+        """カスタムユーザーモデル
+
+        usernameを使わず、emailアドレスをユーザー名として使うようにしています。
+        """
+        email = models.EmailField(_('email address'), unique=True)
+        first_name = models.CharField(_('first name'), max_length=30, blank=True)
+        last_name = models.CharField(_('last name'), max_length=150, blank=True)
+
+        is_staff = models.BooleanField(
+            _('staff status'),
+            default=False,
+            help_text=_(
+                'Designates whether the user can log into this admin site.'),
+        )
+        is_active = models.BooleanField(
+            _('active'),
+            default=True,
+            help_text=_(
+                'Designates whether this user should be treated as active. '
+                'Unselect this instead of deleting accounts.'
+            ),
+        )
+        date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+        objects = UserManager()
+
+        EMAIL_FIELD = 'email'
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = []
+
+        class Meta:
+            verbose_name = _('user')
+            verbose_name_plural = _('users')
+
+        def get_full_name(self):
+            """Return the first_name plus the last_name, with a space in
+            between."""
+            full_name = '%s %s' % (self.first_name, self.last_name)
+            return full_name.strip()
+
+        def get_short_name(self):
+            """Return the short name for the user."""
+            return self.first_name
+
+        def email_user(self, subject, message, from_email=None, **kwargs):
+            """Send an email to this user."""
+            send_mail(subject, message, from_email, [self.email], **kwargs)
+
+        @property
+        def username(self):
+            return self.email
+
+
+    class Person(models.Model):
+        """要員モデル"""
+
+        # 名前
+        name = models.CharField(max_length=255)
+        # メールアドレス
+        email = models.CharField(max_length=255,unique=True)
+
+    class Cost(models.Model):
+        """コストモデル"""
+
+        # 人
+        person = models.ForeignKey('Person', on_delete=models.CASCADE)
+        # 所属会社
+        company = models.IntegerField()
+        # 社員等級
+        grade = models.IntegerField()
+        # 同一部署フラグ（true=製造第一 false=それ以外）
+        is_mf1 = models.BooleanField()
+        # 外注費
+        cost = models.IntegerField()
+        # 適用開始年月
+        start_ym = models.DateTimeField()
+        # 適用終了年月
+        end_ym = models.DateTimeField(null=True, blank=True)
+
+    class Grade(models.Model):
+        """コストモデル"""
+
+        # KSC社員級（同一部署）
+        IN_G1 = 1
+        IN_G2 = 2
+        IN_G3 = 3
+        IN_G4 = 4
+        IN_G5 = 5
+        IN_G6 = 6
+        IN_G7 = 7
+
+        # KSC社員級（他部署）
+        OUT_G1 = 11
+        OUT_G2 = 12
+        OUT_G3 = 13
+        OUT_G4 = 14
+        OUT_G5 = 15
+        OUT_G6 = 16
+        OUT_G7 = 17
+
+
+        # グレード
+        grade = models.IntegerField()
+        # 適用開始年月
+        start_ym = models.DateTimeField()
+        # 適用終了年月
+        end_ym = models.DateTimeField(null=True, blank=True)
---
+++ cms/views.py
@@ -0,0 +1,200 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+)
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import Http404
+from django.shortcuts import redirect, resolve_url, render, get_object_or_404
+from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import generic
+from .forms import (
+    LoginForm, UserCreateForm, UserUpdateForm, MyPasswordChangeForm,
+    MyPasswordResetForm, MySetPasswordForm, PersonForm
+)
+from cms.models import Person
+
+User = get_user_model()
+
+
+class Top(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'cms/top.html'
+
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = 'cms/login.html'
+
+
+class Logout(LoginRequiredMixin, LogoutView):
+    """ログアウトページ"""
+    template_name = 'cms/login.html'
+
+
+class UserCreate(generic.CreateView):
+    """ユーザー仮登録"""
+    template_name = 'cms/user_create.html'
+    form_class = UserCreateForm
+
+    def form_valid(self, form):
+        """仮登録と本登録用メールの発行."""
+        # 仮登録と本登録の切り替えは、is_active属性を使うと簡単です。
+        # 退会処理も、is_activeをFalseにするだけにしておくと捗ります。
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        # 管理サイトのパスワードリセットの際に行っている処理とほぼ同じ
+        current_site = get_current_site(self.request)
+        domain = current_site.domain
+        context = {
+            'protocol': 'https' if self.request.is_secure() else 'http',
+            'domain': domain,
+            'uidb64': urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),
+            'token': urlsafe_base64_encode(force_bytes(user.email)).decode('utf-8'),
+            'user': user,
+        }
+
+        subject_template = get_template('cms/mail_template/create/subject.txt')
+        subject = subject_template.render(context)
+
+        message_template = get_template('cms/mail_template/create/message.txt')
+        message = message_template.render(context)
+
+        user.email_user(subject, message)
+        return redirect('cms:user_create_done')
+
+
+class UserCreateDone(generic.TemplateView):
+    """ユーザー仮登録したよ"""
+    template_name = 'cms/user_create_done.html'
+
+
+class UserCreateComplete(generic.TemplateView):
+    """メール内URLアクセス後のユーザー本登録"""
+    template_name = 'cms/user_create_complete.html'
+
+    def get(self, request, **kwargs):
+        """uid、tokenが正しければ本登録."""
+        token = kwargs.get('token')
+        uidb64 = kwargs.get('uidb64')
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            email = force_text(urlsafe_base64_decode(token))
+            user = User.objects.get(pk=uid, email=email)
+            if user.is_active:
+                raise Http404
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            pass
+        else:
+            user.is_active = True
+            user.save()
+            return super().get(request, **kwargs)
+
+        raise Http404
+
+
+class OnlyYouMixin(LoginRequiredMixin, UserPassesTestMixin):
+    """本人か、スーパーユーザーだけユーザーページアクセスを許可する"""
+    raise_exception = True
+
+    def test_func(self):
+        user = self.request.user
+        return user.pk == self.kwargs['pk'] or user.is_superuser
+
+
+class UserDetail(OnlyYouMixin, generic.DetailView):
+    """ユーザーの詳細ページ"""
+    model = User
+    template_name = 'cms/user_detail.html'  # デフォルトユーザーを使う場合に備え、きちんとtemplate名を書く
+
+
+class UserUpdate(OnlyYouMixin, generic.UpdateView):
+    """ユーザー情報更新ページ"""
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'cms/user_form.html'  # デフォルトユーザーを使う場合に備え、きちんとtemplate名を書く
+
+    def get_success_url(self):
+        return resolve_url('cms:user_detail', pk=self.kwargs['pk'])
+
+
+class PasswordChange(LoginRequiredMixin, PasswordChangeView):
+    """パスワード変更ビュー"""
+    form_class = MyPasswordChangeForm
+    success_url = reverse_lazy('cms:password_change_done')
+    template_name = 'cms/password_change.html'
+
+
+class PasswordChangeDone(LoginRequiredMixin, PasswordChangeDoneView):
+    """パスワード変更しました"""
+    template_name = 'cms/password_change_done.html'
+
+
+class PasswordReset(LoginRequiredMixin, PasswordResetView):
+    """パスワード変更用URLの送付ページ"""
+    subject_template_name = 'cms/mail_template/reset/subject.txt'
+    email_template_name = 'cms/mail_template/reset/message.txt'
+    template_name = 'cms/password_reset_form.html'
+    form_class = MyPasswordResetForm
+    success_url = reverse_lazy('cms:password_reset_done')
+
+
+class PasswordResetDone(LoginRequiredMixin, PasswordResetDoneView):
+    """パスワード変更用URLを送りましたページ"""
+    template_name = 'cms/password_reset_done.html'
+
+
+class PasswordResetConfirm(LoginRequiredMixin, PasswordResetConfirmView):
+    """新パスワード入力ページ"""
+    form_class = MySetPasswordForm
+    success_url = reverse_lazy('cms:password_reset_complete')
+    template_name = 'cms/password_reset_confirm.html'
+
+
+class PasswordResetComplete(LoginRequiredMixin, PasswordResetCompleteView):
+    """新パスワード設定しましたページ"""
+    template_name = 'cms/password_reset_complete.html'
+
+
+class PersonList(LoginRequiredMixin, generic.TemplateView):
+
+    def person_list(request):
+        """要員の一覧"""
+        persons = Person.objects.all().order_by('id')
+        return render(request,
+                      'cms/person_list.html',     # 使用するテンプレート
+                      {'persons': persons})         # テンプレートに渡すデータ
+
+    def person_edit(request, person_id=None):
+        """要員の編集"""
+    #     return HttpResponse('要員の編集')
+        if person_id:   # person_id が指定されている (修正時)
+            person = get_object_or_404(Person, pk=person_id)
+        else:         # person_id が指定されていない (追加時)
+            person = Person()
+
+        if request.method == 'POST':
+            form = PersonForm(request.POST, instance=person)  # POST された request データからフォームを作成
+            if form.is_valid():    # フォームのバリデーション
+                person = form.save(commit=False)
+                person.save()
+                return redirect('cms:person_list')
+        else:    # GET の時
+            form = PersonForm(instance=person)  # person インスタンスからフォームを作成
+
+        return render(request, 'cms/person_edit.html', dict(form=form, person_id=person_id))
+
+    def person_del(request, person_id):
+        """要員の削除"""
+    #     return HttpResponse('要員の削除')
+        person = get_object_or_404(Person, pk=person_id)
+        person.delete()
+        return redirect('cms:person_list')
+
+
